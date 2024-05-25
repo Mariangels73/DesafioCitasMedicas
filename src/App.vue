66f4d32d995df865ef56bd46de5c4d3f7502e61c
Maria de los Angeles Loyo <110129762+Mariangels73@users.noreply.github.com>
2024-05-25 00:45:04 -0400

    Desafio Citas Medicas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,139 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <form @submit.prevent="agregarConsulta">
+      <main>
+        <div>
+          <label :class="{ 'text-red': !consulta.paciente }">Paciente</label>
+          <input v-model="consulta.paciente" />
+        </div>
+        <div>
+          <label :class="{ 'text-red': !consulta.fecha }">Fecha</label>
+          <input type="date" v-model="consulta.fecha" />
+        </div>
+        <div>
+          <label :class="{ 'text-red': !consulta.hora }">Hora</label>
+          <input type="time" v-model="consulta.hora" />
+        </div>
+        <div>
+          <label :class="{ 'text-red': !consulta.gravedad }">Gravedad</label>
+          <select v-model="consulta.gravedad">
+            <option value="1">Baja</option>
+            <option value="2">Media</option>
+            <option value="3">Alta</option>
+          </select>
+        </div>
+        <div>
+          <label :class="{ 'text-red': !consulta.motivo }">Motivo</label>
+          <input v-model="consulta.motivo" />
+        </div>
+      </main>
+      <button
+        :disabled="
+          !consulta.paciente ||
+          !consulta.fecha ||
+          !consulta.hora ||
+          !consulta.motivo ||
+          !consulta.gravedad
+        "
+      >
+        Agregar
+      </button>
+    </form>
+    <div class="consultas">
+      <p class="text-red" v-if="!consultas.length">
+        Aún no hay consultas registradas
+      </p>
+      <TablaComponente
+        v-else
+        :consultas="consultas"
+        @eliminarConsulta="eliminarConsulta"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaComponente from "./components/TablaComponente";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      consultas: [],
+      consulta: {},
+    };
+  },
+  methods: {
+    agregarConsulta() {
+      const { consulta } = this;
+      this.consultas.push(consulta);
+      this.consulta = {};
+    },
+    eliminarConsulta(i) {
+      this.consultas.splice(i, 1);
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    TablaComponente,
+  },
+};
 </script>
 
 <style>
+body{
+  background-color: rgb(252, 245, 245);
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+}
+
+.text-red {
+  color: red;
+}
+
+form {
+  border: ridge 3px;
+  border-radius: 10px;
+  padding: 10px;
+  margin: auto;
+  max-width: 650px;
+}
+
+form main {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
+form div {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 5px;
+}
+
+form label {
+  font-weight: bold;
+}
+
+form select,
+form input {
+  height: 20px;
+  box-sizing: border-box;
+}
+
+form button {
+  margin-top: 20px;
+}
+
+.consultas {
+  margin: 20px auto;
+  width: fit-content;
 }
 </style>
---
+++ src/components/TablaComponente.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <table border="ridge">
+      <thead>
+        <tr>
+          <th>Paciente</th>
+          <th>Fecha</th>
+          <th>Hora</th>
+          <th>Motivo</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="({ paciente, fecha, hora, motivo, gravedad }, i) in consultas"
+          :key="i"
+          :class="{
+            'bg-red': gravedad == 3,
+            'bg-yellow': gravedad == 2,
+            'bg-green': gravedad == 1,
+          }"
+        >
+          <td>{{ paciente }}</td>
+          <td>{{ fecha }}</td>
+          <td>{{ hora }}</td>
+          <td>{{ motivo }}</td>
+          <td>
+            <button @click="$emit('eliminarConsulta', i)">Eliminar</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["consultas"],
+};
+</script>
+
+<style>
+td,
+th {
+  padding: 10px;
+}
+
+.bg-red {
+  background: red;
+  color: white;
+}
+.bg-yellow {
+  background: yellow;
+}
+.bg-green {
+  background: greenyellow;
+}
+</style>
